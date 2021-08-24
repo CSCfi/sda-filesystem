8392188a4c84c81bf188d024cb5be8d511e9600e
Stefan Negru <stefan@blankdots.com>
2021-08-24 15:06:47 +0300

    fixes to the setup so all dependencies are there

--- dev_utils/setup-linux.sh
+++ dev_utils/setup-linux.sh
@@ -6,17 +6,30 @@ OS=$(uname -nsm)
 
 # Check terraform exists and it has a specific version
 if ! [ -x "$(command -v go)" ]; then
-    echo 'terraform is not installed, and it is a requirement.' >&2
+    echo 'golang is not installed, and it is a requirement.' >&2
     exit 1
 elif ! [ "$(go version | awk 'NR==1{print $3}')" == "${GO_VERSION}" ]; then
-    echo "The terraform version on this ${OS} machine is different than the one required: ${GO_VERSION}" >&2
+    echo "The golang version on this ${OS} machine is different than the one required: ${GO_VERSION}" >&2
     echo "continuing for now ..."
 fi
 
 
-export GO111MODULE=off; go get -v github.com/therecipe/qt/cmd/... 
+# download as general package
+# otherwise it will not work
+export GO111MODULE=off
 
-export GO111MODULE=on; go install -v -tags=no_env github.com/therecipe/qt/cmd/... && \
-    go mod vendor && rm -rf vendor/github.com/therecipe/env_linux_amd64_513 && \
-    git clone https://github.com/therecipe/env_linux_amd64_513.git vendor/github.com/therecipe/env_linux_amd64_513 && \
-    "$(go env GOPATH)"/bin/qtsetup
+go get github.com/therecipe/qt/cmd/... 
+
+export GO111MODULE=on
+
+# dependencies not easily installed
+go get github.com/therecipe/qt/internal/binding/files/docs/5.12.0
+go get github.com/therecipe/qt/internal/cmd/moc@v0.0.0-20200904063919-c0c124a5770d
+
+go install -v -tags=no_env github.com/therecipe/qt/cmd/...
+
+go mod vendor && rm -rf vendor/github.com/therecipe/env_linux_amd64_513
+
+git clone https://github.com/therecipe/env_linux_amd64_513.git vendor/github.com/therecipe/env_linux_amd64_513
+
+"$(go env GOPATH)"/bin/qtsetup
