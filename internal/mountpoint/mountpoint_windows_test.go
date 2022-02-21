fee057a5bfd933b5cbc276f43467fb81e97f3d3a
Emmi Rehn <emmi.rehn@csc.fi>
2022-02-21 13:50:16 +0200

    Windows version now accepts also empty folders as mount points (code removes the directory before mounting)

--- internal/mountpoint/mountpoint_windows_test.go
+++ internal/mountpoint/mountpoint_windows_test.go
@@ -1,6 +1,7 @@
 package mountpoint
 
 import (
+	"github.com/hectane/go-acl"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -11,14 +12,40 @@ func TestCheckMountPoint(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to create folder: %s", err.Error())
 	}
+	defer os.RemoveAll(node)
 
-	if err = CheckMountPoint(node); err == nil {
-		t.Fatal("Function did not return error when folder existed")
+	subnode := node + string(os.PathSeparator) + "subdir"
+	if err = os.Mkdir(subnode, 0755); err != nil {
+		t.Fatalf("Failed to create folder: %s", err.Error())
 	}
 
-	os.RemoveAll(node)
+	if err = CheckMountPoint(node); err == nil {
+		t.Fatal("Function did not return error when folder was not empty")
+	}
+
+	if err = CheckMountPoint(subnode); err != nil {
+		t.Fatalf("Function returned error for empty folder: %s", err.Error())
+	}
 
 	if err = CheckMountPoint(node); err != nil {
 		t.Fatalf("Function returned error when folder did not exist: %s", err.Error())
+	}
+
+	if err = CheckMountPoint(subnode); err != nil {
+		t.Fatalf("Function returned error when path to folder did not exist: %s", err.Error())
+	}
+}
+
+func TestCheckMountPoint_Fail_MkdirAll(t *testing.T) {
+	node, err := ioutil.TempDir("", "dir")
+	if err != nil {
+		t.Fatalf("Failed to create folder: %s", err.Error())
+	}
+	defer os.RemoveAll(node)
+
+	if err = acl.Chmod(node, 0444); err != nil {
+		t.Errorf("Changing permission bits failed: %s", err.Error())
+	} else if err = CheckMountPoint(node + filepath.FromSlash("/child/grandchild")); err == nil {
+		t.Error("Function should have returned non-nil error")
 	}
 }
