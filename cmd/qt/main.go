02e226d4d76068f90fab039a1250d20bdcccd70c
Stefan Negru <stefan@blankdots.com>
2022-03-29 07:58:58 +0300

    set version to 1.1.0

--- cmd/qt/main.go
+++ cmd/qt/main.go
@@ -185,7 +185,7 @@ func main() {
 	core.QCoreApplication_SetApplicationName("Data Gateway")
 	core.QCoreApplication_SetOrganizationName("CSC")
 	core.QCoreApplication_SetOrganizationDomain("csc.fi")
-	core.QCoreApplication_SetApplicationVersion("1.0.0")
+	core.QCoreApplication_SetApplicationVersion("1.1.0")
 	core.QCoreApplication_SetAttribute(core.Qt__AA_EnableHighDpiScaling, true)
 
 	gui.NewQGuiApplication(len(os.Args), os.Args)
