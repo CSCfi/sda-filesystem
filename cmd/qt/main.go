70f86d4ee5fabfb32b14da178f1dfd851b676297
Emmi Rehn <emmi.rehn@csc.fi>
2022-04-07 18:32:22 +0300

    Update application version in qt

--- cmd/qt/main.go
+++ cmd/qt/main.go
@@ -198,7 +198,7 @@ func main() {
 	core.QCoreApplication_SetApplicationName("Data Gateway")
 	core.QCoreApplication_SetOrganizationName("CSC")
 	core.QCoreApplication_SetOrganizationDomain("csc.fi")
-	core.QCoreApplication_SetApplicationVersion("1.1.0")
+	core.QCoreApplication_SetApplicationVersion("1.2.0")
 	core.QCoreApplication_SetAttribute(core.Qt__AA_EnableHighDpiScaling, true)
 
 	gui.NewQGuiApplication(len(os.Args), os.Args)
